520f42dccd8da62366e779d5ae4dc333a1a7e6e7
camilo_ici <c.cifuentes14@ufromail.cl>
2026-06-24 14:28:35 -0400

    style(backend): Cambio de nombre de parámetros
    - Se corrigen los parámetros del archivo de configuración a mayúsculas
    - Se cambia el nombre de delta_epsilon a EPSILON_DECAY por convención

--- backend/configuracion.py
+++ backend/configuracion.py
@@ -1,26 +1,26 @@
-n_estados = 3
+N_ESTADOS = 3
 # 0 = Okay, 1 = Sospechoso, 2 = Peligroso
 
-n_acciones = 3
+N_ACCIONES = 3
 # 0 = Permitir, 1 = Investigar, 2 = Bloquear
 
-alpha = 0.1 # Tasa de aprendizaje
-gamma = 0.95 # Importancia del futuro
-epsilon = 0.2 # Exploración inicial
+ALPHA = 0.1 # Tasa de aprendizaje
+GAMMA = 0.95 # Importancia del futuro
+EPSILON = 0.2 # Exploración inicial
 
-epsilon_min = 0.05 # Mínimo valor de épsilon
-delta_epsilon = 0.995 # Factor de decrecimiento de épsilon por episodio
+EPSILON_MIN = 0.05 # Mínimo valor de épsilon
+EPSILON_DECAY = 0.995 # Factor de decrecimiento de épsilon por episodio
 
-recompensa_okay_bien = 2
-recompensa_okay_mal = -5
+RECOMPENSA_OKAY_BIEN = 2
+RECOMPENSA_OKAY_MAL = -5
 
-recompensa_sospechoso_bien = 5
-recompensa_sospechoso_mal = -2
+RECOMPENSA_SOSPECHOSO_BIEN = 5
+RECOMPENSA_SOSPECHOSO_MAL = -2
 
-recompensa_peligroso_bien = 10
-recompensa_peligroso_mal = -10
+RECOMPENSA_PELIGROSO_BIEN = 10
+RECOMPENSA_PELIGROSO_MAL = -10
 
-episodios = 500
-pasos_por_episodio = 50
+EPISODIOS = 500
+PASOS_POR_EPISODIO = 50
 
-cors_origins = ["*"] # Permitir solicitudes desde cualquier origen
+CORS_ORIGINS = ["*"] # Permitir solicitudes desde cualquier origen
