78423d3a899f0b1db2d9e951c597a173b20f80af
camilo_ici <c.cifuentes14@ufromail.cl>
2026-06-24 11:53:41 -0400

    Initial commit

---
+++ backend/qlearning.py
@@ -0,0 +1,44 @@
+import numpy as np
+import random
+
+class QLearningAgent:
+    def __init__(
+        self,
+        n_estados, # Estados del agente
+        n_acciones, # Acciones
+        tasa_aprendizaje=0.1, # Alfa. Peso de la información frente a la experiencia antigua
+        factor_descuento=0.95, # Gamma. Peso de las recompensas futuras frente a las inmediatas
+        epsilon=0.2 # Equilibra exploración con explotación
+    ):
+
+        self.n_estados = n_estados
+        self.n_acciones = n_acciones
+
+        self.alpha = tasa_aprendizaje
+        self.gamma = factor_descuento
+        self.epsilon = epsilon
+
+        # Tabla Q
+        self.q_table = np.zeros((n_estados, n_acciones))
+
+    def elegir_accion(self, estado):
+
+        # Exploración
+        if random.uniform(0, 1) < self.epsilon:
+            return random.randint(0, self.n_acciones - 1)
+
+        # Explotación
+        return np.argmax(self.q_table[estado])
+
+    def actualizar(self, estado, accion, recompensa, proximo_estado):
+        original = self.q_table[estado, accion] # Q(s, a)
+
+        maximo_siguiente = np.max(self.q_table[proximo_estado]) # max Q(s', a')
+
+        nuevo_valor = original + self.alpha * (recompensa + self.gamma * maximo_siguiente - original)
+        # Q(s, a) = Q(s, a) + lr(R + gamma*max(Q(s', a')) - Q(s, a))
+
+        self.q_table[estado, accion] = nuevo_valor
+
+    def get_q_table(self):
+        return self.q_table.tolist()
