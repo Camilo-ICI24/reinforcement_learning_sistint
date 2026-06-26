7095d17d64ac4ce547eeffca2657efbf7100c491
camilo_ici <c.cifuentes14@ufromail.cl>
2026-06-26 02:09:49 -0400

    style(ui): Se agregan trivias y frases para la pantalla de carga

---
+++ frontend/datos/curiosidades.js
@@ -0,0 +1,16 @@
+const curiosidades = [
+    "Un agente de aprendizaje por refuerzo aprende probando acciones y recibiendo recompensas.",
+    "¿Sabías que detectar anomalías es como notar que alguien le puso piña a la pizza?",
+    "Una Q-Table guarda qué tan buena parece cada acción en cada estado.",
+    "Cargando neuronas imaginarias... prometen llegar peinadas.",
+    "Si el modelo no converge, al menos conversemos nosotros.",
+    "Para cuando esta carga termine, casi 2400 pizzas serán ordenadas en el mundo.",
+    "¿En serio alguien se va a dar la molestia de leer esto?",
+    "Correcto: Suma puntos. Incorrecto: Penitencia",
+    "Para cuando esta carga termine, 500 TB de información habrán viajado por la red.",
+    "Ingeniería no es artesanía",
+    "Cómo funciona el aprendizaje por refuerzo google buscar",
+    "Para cuando esta carga termine, 300 autos habrán estado circulando por Santiago.",
+    "¿Es más alto el Monte Everest, el Costanera Center, o el nivel de cortisol del programador?",
+    "Gracias Gacitúa por permitir el desarrollo de esta propuesta"
+];
